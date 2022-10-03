e77933478c81e7a7bd01f9f1173e87664d8281a9
Vinicius <vianaxd99@gmail.com>
2022-10-03 07:15:17 -0300

    games controller fix

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -24,6 +24,7 @@ export async function insertGame (req, res) {
     try {
         await connection.query(`INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1, $2, $3, $4, $5)`,
         [name, image, stockTotal, categoryId, pricePerDay]);
+        res.sendStatus(201);
     } catch (err) {
         res.status(500).send(err);
     }
