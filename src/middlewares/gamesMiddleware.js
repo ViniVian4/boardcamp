1171cb8a24de92590473140a0d6dfe32c4b4fe43
Vinicius <vianaxd99@gmail.com>
2022-10-03 07:09:32 -0300

    games middleware done

--- src/middlewares/gamesMiddleware.js
+++ src/middlewares/gamesMiddleware.js
@@ -1,1 +1,39 @@
 import connection from '../db/database.js';
+import gameSchema from '../schemas/gameSchema.js';
+
+async function verifyGame(req, res, next) {
+    const newGame = req.body;
+
+    const validation = gameSchema.validate(newGame, { abortEarly: true });
+    if (validation.error) {
+        res.status(400).send(validation.error);
+        return;
+    }
+
+    try {
+        if (newGame.categoryId) {
+            const category = await connection.query(
+                `SELECT * FROM categories WHERE id=$1`, [newGame.categoryId]
+            );
+
+            if (category.rows.length === 0) {
+                res.sendStatus(400);
+                return;
+            }
+        }
+
+        const name = await connection.query(
+            `SELECT * FROM games WHERE LOWER(name)=$1`, [newGame.name]
+        );
+        if (name.rows.length === 0) {
+            res.sendStatus(409);
+            return;
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+
+    next();
+}
+
+export default verifyGame;
